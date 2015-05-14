7ca05be9223881e66f66fd940a8d6dcff9165aa6
J.B. Zimmerman <jbz@ronin7.com>
2015-05-13 22:08:27 -0400

    Fixed so it won't return a tag already generated in the same run.  Had to simplify re.split to single test to get it to work, unsure why.

--- tag_gen.py
+++ tag_gen.py
@@ -8,18 +8,24 @@ parser = argparse.ArgumentParser()
 parser.add_argument("numtags", help="number of tags to generate", type=int)
 parser.add_argument("prefixes", help="file containing prefix words, one per line")
 parser.add_argument("colors", help="file containing colors, one per line")
-parser.add_argument("current", help="file with list of currently existing tags, if any")
+parser.add_argument("-x", "--exclude", help="file with list of tags to exclude, if any", type=str)
 parser.add_argument("-u", "--uppercase", help="output tags in uppercase", action="store_true")
 args = parser.parse_args()
 
-currentlist = args.current
 colorlist = args.colors
 prefixlist = args.prefixes
 
 prefixes = []
 colors = []
-existing = []
 output = []
+existing = []
+
+if args.exclude:
+  currentlist = args.exclude
+  with open(currentlist) as f:
+	existing = [re.split('_', tag) for tag in f.read().splitlines()]
+#  print existing
+
 
 with open(prefixlist) as f:
 	prefixes = f.read().splitlines()
@@ -27,21 +33,18 @@ with open(prefixlist) as f:
 with open(colorlist) as f:
 	colors = f.read().splitlines()
 
-with open(currentlist) as f:
-	existing = [re.split('\s_,|/]+', tag) for tag in f.read().splitlines()]
-
-
 def generate_tag(n):
 	tag = []
 	while len(output) < n:
 		tag = [ random.choice(prefixes), random.choice(colors) ]
 #		print "Trying " + str(tag)
-		if tag not in existing:
-#			print "OK"
-			output.append(tag)
+		if tag not in output:
+			if tag not in existing:
+				output.append(tag)
+#			else:
+#				print "Failed, exists in excludes!"
 #		else:
-#			print "Failed, exists!"
-			 
+#			print "Failed, already generated!" 
 
 generate_tag(args.numtags)
 
